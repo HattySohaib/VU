3e42ff63688f62792259357de462a5d89d514e80
Sohaib Aftab <hattysohaib@gmail.com>
2024-07-15 02:17:54 +0530

    realised my mistake

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-document.getElementById("sign-in-btn").addEventListener("click", function () {
-  // Read input values
-  console.log("Sign in attempted");
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  // Predefined credentials
-  const validUsername = "181011072";
-  const validPassword = "tasfiya";
+    // Read input values
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-  // Check credentials
-  if (username === validUsername && password === validPassword) {
-    // Redirect on successful login
-    window.location.href = "./result.html";
-  } else {
-    alert("Invalid username or password");
-  }
-});
+    // Predefined credentials
+    const validUsername = "181011072";
+    const validPassword = "tasfiya";
+
+    // Check credentials
+    if (username === validUsername && password === validPassword) {
+      // Redirect on successful login
+      window.location.href = "./result.html";
+    } else {
+      alert("Invalid username or password");
+    }
+  });
