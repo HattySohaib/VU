5d190b24e77df74f2df1e59df238001e7cf9add8
Sohaib Aftab <hattysohaib@gmail.com>
2024-07-15 01:54:08 +0530

    add all

---
+++ script.js
@@ -0,0 +1,21 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Read input values
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Predefined credentials
+    const validUsername = "181011072";
+    const validPassword = "tasfiya";
+
+    // Check credentials
+    if (username === validUsername && password === validPassword) {
+      // Redirect on successful login
+      window.location.href = "index.html";
+    } else {
+      alert("Invalid username or password");
+    }
+  });
