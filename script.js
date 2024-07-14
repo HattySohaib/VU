132b8b24069d22c1c2a6b207cd9c7a46a9d9092d
Sohaib Aftab <hattysohaib@gmail.com>
2024-07-15 02:16:12 +0530

    fixed js script further 2

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-document
-  .getElementById("sign-in-btn")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    // Read input values
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+document.getElementById("sign-in-btn").addEventListener("click", function () {
+  // Read input values
+  console.log("Sign in attempted");
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-    // Predefined credentials
-    const validUsername = "181011072";
-    const validPassword = "tasfiya";
+  // Predefined credentials
+  const validUsername = "181011072";
+  const validPassword = "tasfiya";
 
-    // Check credentials
-    if (username === validUsername && password === validPassword) {
-      // Redirect on successful login
-      window.location.href = "./result.html";
-    } else {
-      alert("Invalid username or password");
-    }
-  });
+  // Check credentials
+  if (username === validUsername && password === validPassword) {
+    // Redirect on successful login
+    window.location.href = "./result.html";
+  } else {
+    alert("Invalid username or password");
+  }
+});
